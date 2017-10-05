11b8dc19d10e6ab985407fdc136b7260989a4f21
aaizuss <aaizuss@gmail.com>
2017-10-05 15:05:39 -0700

    Add test constants file (whoops)

---
+++ src/test/java/com/aaizuss/TestConstants.java
@@ -0,0 +1,7 @@
+package com.aaizuss;
+
+public class TestConstants {
+    public static String USER_DIR = System.getProperty("user.dir");
+    public static String TEST_DIR = System.getProperty("user.dir") + "/test-directory/";
+    public static String PUBLIC_DIR = System.getProperty("user.dir") + "/public/";
+}
